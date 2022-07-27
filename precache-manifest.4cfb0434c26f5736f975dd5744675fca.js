99d9a4edb001216bb15cb1c965bcffb0d4c96cc3
adarshc2022 <110046953+adarshc2022@users.noreply.github.com>
2022-07-27 19:22:39 +0530

    ufirst

---
+++ precache-manifest.4cfb0434c26f5736f975dd5744675fca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e8cc8ad9a2a0c3f1ba",
+    "url": "/static/css/main.10b0a919.chunk.css"
+  },
+  {
+    "revision": "50e8cc8ad9a2a0c3f1ba",
+    "url": "/static/js/main.50e8cc8a.chunk.js"
+  },
+  {
+    "revision": "c31e58be25f557e4fa6b",
+    "url": "/static/js/1.c31e58be.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5d1fc7572d26d8ceb77ed5ce5283d55",
+    "url": "/index.html"
+  }
+];
